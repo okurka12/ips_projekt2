a8cbb3c29c144f92fcddbe7890c033b4fa2ae74d
Vit Pavlik <pavlikvitek13@gmail.com>
2023-12-03 08:49:14 +0100

    first commit

---
+++ aut.c
@@ -0,0 +1,15 @@
+/*****************
+**  Vit Pavlik  **
+**   xpavli0a   **
+**    251301    **
+*****************/
+
+/**
+ * Reseni druhe ulohy IPS 2023/2024
+*/
+
+#include <threads.h>
+
+int main() {
+    mtx_t ahoj;
+}
